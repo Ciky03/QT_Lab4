b6eeb84e7a8ef4f4614788feb9e152c59ece064a
Ciky03 <Ciky03@users.noreply.github.com>
2024-12-10 19:00:44 +0800

    客户端解析json数据

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,89 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QJsonValue>
+#include <QJsonObject>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentWidget(ui->loginPage);
+    m_chatClient = new ChatClient(this);
+
+    connect(m_chatClient, &ChatClient::connected, this, &MainWindow::connecedToServer);
+    // connect(m_chatClient, &ChatClient::messageReceived, this, &MainWindow::messageReceived);
+    connect(m_chatClient, &ChatClient::jsonReceived, this, &MainWindow::jsonReceived);
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_loginBtn_clicked()
+{
+    m_chatClient->connectToServer(QHostAddress(ui->serverEdit->text()), 1967);
+}
+
+
+void MainWindow::on_sayBtn_clicked()
+{
+    if (!ui->sayLineEdit->text().isEmpty()) {
+        m_chatClient->sendMessage(ui->sayLineEdit->text());
+    }
+}
+
+
+void MainWindow::on_logoutBtn_clicked()
+{
+    m_chatClient->disconnectFromHost();
+    ui->stackedWidget->setCurrentWidget(ui->loginPage);
+}
+
+void MainWindow::connecedToServer()
+{
+    ui->stackedWidget->setCurrentWidget(ui->chatPage);
+    m_chatClient->sendMessage(ui->usernameEdit->text(), "login");
+
+}
+
+void MainWindow::messageReceived(const QString &sender, const QString &msg)
+{
+    ui->roomTextEdit->append(QString("%1 : %2").arg(sender).arg(msg));
+}
+
+void MainWindow::jsonReceived(const QJsonObject &docobj)
+{
+    const QJsonValue typeVal = docobj.value("type");
+    if (typeVal.isNull() || !typeVal.isString())
+        return;
+
+    if (typeVal.toString().compare("message", Qt::CaseInsensitive) == 0) {
+        const QJsonValue textVal = docobj.value("text");
+        const QJsonValue senderVal = docobj.value("sender");
+
+        if (textVal.isNull() || !textVal.isString())
+            return;
+        if (senderVal.isNull() || !senderVal.isString())
+            return;
+
+        messageReceived(senderVal.toString(), textVal.toString());
+
+    } else if (typeVal.toString().compare("newuser", Qt::CaseInsensitive) == 0) {
+        const QJsonValue usernameVal = docobj.value("username");
+        if (usernameVal.isNull() || !usernameVal.isString())
+            return;
+
+        userJoined(usernameVal.toString());
+
+    }
+}
+
+void MainWindow::userJoined(const QString &user)
+{
+    ui->userListWidget->addItem(user);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "chatclient.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +18,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_loginBtn_clicked();
+
+    void on_sayBtn_clicked();
+
+    void on_logoutBtn_clicked();
+
+    void connecedToServer();
+
+    void messageReceived(const QString &sender, const QString &msg);
+
+    void jsonReceived(const QJsonObject &docobj);
+
+    void userJoined(const QString &user);
 private:
     Ui::MainWindow *ui;
+
+    ChatClient *m_chatClient;
 };
 #endif // MAINWINDOW_H
